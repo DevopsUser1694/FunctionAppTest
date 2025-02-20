3870db9d1891540b8e61240a153156d882a2f13e
Chandan Kumar <jilimjahim007@outlook.com>
2025-02-21 01:37:59 +0530

    Add project files.

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,82 @@
+﻿using FunctionAppTest.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppTest.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                ProductName = "DefaultP1",
+                CreatedBy = "Chandan",
+                CreatedOn = DateTime.Now,
+                ModifiedBy = "Chandan",
+                ModifiedOn = DateTime.Now,
+
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                ProductName = "DefaultP2",
+                CreatedBy = "Amit",
+                CreatedOn = DateTime.Now,
+                ModifiedBy = "Amit",
+                ModifiedOn = DateTime.Now,
+
+            });
+
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 1,
+                ItemName = "Item_P1_I1",
+                ProductId = 1,
+                Count = 1,
+            });
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 2,
+                ItemName = "Item_P1_I2",
+                ProductId = 1,
+                Count = 2,
+            });
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 3,
+                ItemName = "Item_P1_I3",
+                ProductId = 1,
+                Count = 3,
+            });
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 4,
+                ItemName = "Item_P2_I1",
+                ProductId = 2,
+                Count = 1,
+            });
+            modelBuilder.Entity<Item>().HasData(new Item
+            {
+                Id = 5,
+                ItemName = "Item_P2_I2",
+                ProductId = 2,
+                Count = 2,
+            });
+        }
+
+    }
+}
---
+++ Migrations/20250220085758_InitialSetup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FunctionAppTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CreatedBy", "CreatedOn", "ModifiedBy", "ModifiedOn", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, "Chandan", new DateTime(2025, 2, 20, 14, 27, 58, 491, DateTimeKind.Local).AddTicks(8587), "Chandan", new DateTime(2025, 2, 20, 14, 27, 58, 491, DateTimeKind.Local).AddTicks(8602), "DefaultP1" },
+                    { 2, "Amit", new DateTime(2025, 2, 20, 14, 27, 58, 491, DateTimeKind.Local).AddTicks(8673), "Amit", new DateTime(2025, 2, 20, 14, 27, 58, 491, DateTimeKind.Local).AddTicks(8674), "DefaultP2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
